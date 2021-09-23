3ee880ad5ca617d44b0d35a9dbc5196920c0e510
Yan Rodrigues Santos <yyan1207@gmail.com>
2021-09-23 13:43:59 -0300

    Classe Voadores finalizada

--- src/br/com/zup/Voadores.java
+++ src/br/com/zup/Voadores.java
@@ -1,4 +1,22 @@
 package br.com.zup;
 
 public class Voadores extends Vilao {
+    double danoTiro;
+
+    public Voadores (){
+
+    }
+
+    public double getDanoTiro() {
+        return danoTiro;
+    }
+
+    public void setDanoTiro(double danoTiro) {
+        this.danoTiro = danoTiro;
+    }
+
+    public double aplicarDano(){
+        return danoTiro;
+    }
+
 }
