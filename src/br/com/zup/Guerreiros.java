2d9d352534af25935c152702c3c1957f679132bb
Yan Rodrigues Santos <yyan1207@gmail.com>
2021-09-23 13:40:20 -0300

    Classe Guerreiro finalizada

--- src/br/com/zup/Guerreiros.java
+++ src/br/com/zup/Guerreiros.java
@@ -1,5 +1,22 @@
 package br.com.zup;
 
 public class Guerreiros extends Heroi{
+    double poderAtaque;
 
+    public Guerreiros(){
+
+    }
+
+    public double getPoderAtaque() {
+        return poderAtaque;
+    }
+
+    public void setPoderAtaque(double poderAtaque) {
+        this.poderAtaque = poderAtaque;
+    }
+
+    public double aplicarDano(){
+        return poderAtaque;
+    }
 }
+
