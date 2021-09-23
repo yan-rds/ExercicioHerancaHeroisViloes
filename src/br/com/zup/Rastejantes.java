c5dd766aea9aa50964c9f99e24059959adbada37
Yan Rodrigues Santos <yyan1207@gmail.com>
2021-09-23 13:47:08 -0300

    Classe rastejante finalizada

--- src/br/com/zup/Rastejantes.java
+++ src/br/com/zup/Rastejantes.java
@@ -1,4 +1,21 @@
 package br.com.zup;
 
 public class Rastejantes extends Vilao {
+    double danoEspinhos;
+
+    public Rastejantes(){
+
+    }
+    public double getDanoEspinhos() {
+        return danoEspinhos;
+    }
+
+    public void setDanoEspinhos(double danoEspinhos) {
+        this.danoEspinhos = danoEspinhos;
+    }
+
+    public double aplicaDano(){
+        return danoEspinhos;
+    }
+
 }
