53fba6da7c0a2e1fcdc14ae6a7eae57e4768c887
Yan Rodrigues Santos <yyan1207@gmail.com>
2021-09-23 14:10:06 -0300

    Primeiro teste finalizado

--- src/br/com/zup/Main.java
+++ src/br/com/zup/Main.java
@@ -2,6 +2,11 @@ package br.com.zup;
 
 public class Main {
     public static void main(String[] args) {
-        Guerreiros guerreiro = new Guerreiros();
+        Guerreiros guerreiro = new Guerreiros("guerreiro", 100, 30);
+        Voadores voador = new Voadores("voador", 100, 25.5);
+
+        System.out.println(guerreiro.getVida());
+        guerreiro.receberDano(voador.aplicarDano());
+        System.out.println(guerreiro.getVida());
     }
 }
